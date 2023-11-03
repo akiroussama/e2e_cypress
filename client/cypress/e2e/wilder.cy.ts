1a883917a071f3c67eb852f3c28d4e426d9f219a
Akir Oussama <akir.oussama@gmail.com>
2023-11-03 09:06:07 +0100

    ci ok

--- client/cypress/e2e/wilder.cy.ts
+++ client/cypress/e2e/wilder.cy.ts
@@ -3,16 +3,16 @@ describe("SkillsAdmin", () => {
     cy.visit("/");
   });
 
-  it("adds a new skill", () => {
+  it("adds a new wilder", () => {
     cy.get('[data-testid="newWilderName"]')
       .should("be.visible")
       .type("James Bond");
-      cy.get("button").should("be.visible").click();
+    cy.get("button").click();
     // Add assertions to check if the new skill was added correctly
   });
 
-  it("deletes a skill", () => {
-    cy.get('[data-cy="delete-skill"]').should("be.visible").click();
+  it("modify a wilder", () => {
+    cy.get('[name="submission"]').click();
     // Add assertions to check if the skill was deleted correctly
   });
 });
