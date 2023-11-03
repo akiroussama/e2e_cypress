9e1edc54ed2d54c2080471a9e097ac783ef2821f
Akir Oussama <akir.oussama@gmail.com>
2023-11-03 07:29:38 +0100

    use same directory for test

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,16 +1,25 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'junit',
+  reporter: "junit",
+
   reporterOptions: {
-    mochaFile: 'cypress/results/output.xml',
+    mochaFile: "cypress/results/output.xml",
   },
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:8080',
+    baseUrl: "http://localhost:8080",
   },
-})
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+});
